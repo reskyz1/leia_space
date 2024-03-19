9a74f4f191525ef8b929607154c335c1769c7a8b
Ryan Harmes Eskinazi <141065002+reskyz1@users.noreply.github.com>
2024-03-18 21:24:38 -0300

    telas implementadas

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
 
 def index(request):
-        return HttpResponse('<h1>Leia Space</h1>')
+        return render(request, 'galeria/index.html')
+
+def imagem(request):
+        return render(request, 'galeria/imagem.html')
