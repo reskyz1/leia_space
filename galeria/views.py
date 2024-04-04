f49bb095ebb12face0f8100e4d6a9fb56359f874
Ryan Harmes Eskinazi <141065002+reskyz1@users.noreply.github.com>
2024-04-04 15:50:10 -0300

    updates

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+
+
 def index(request):
-        return render(request, 'galeria/index.html')
+        dados = {
+        1:{"nome":"Leia na cama", 
+           "legenda":"03/10/2022 - Recife, PE"},
+        2:{"nome":"Novo medalhão",
+           "legenda":"17/02/2024 - Recife, PE"}
+}
+
+        return render(request, 'galeria/index.html', {"cards":dados})
 
 def imagem(request):
         return render(request, 'galeria/imagem.html')
